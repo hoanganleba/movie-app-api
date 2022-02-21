aa3079d7f43c463391d698bc9e4d83a5c1febaee
Hoang An Le Ba <hoanganleba@gmail.com>
2022-02-22 00:09:58 +0700

    Remove object Id

--- api/profile.js
+++ api/profile.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user.model')
-const mongoose = require('mongoose')
-const { ObjectId } = mongoose.Schema.Types;
 
 router.get('/:id', async (req, res) => {
   const user = await User.findById(req.params.id).exec()
